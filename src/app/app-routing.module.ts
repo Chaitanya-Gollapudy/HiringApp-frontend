a32d7221b1d79ce445f1e4439603aedca1f563a5
Chaitanya Gollapudy <chaitanyagollapudy@gmail.com>
2021-06-14 21:45:40 +0530

    Candidate Remarks

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PanelBoardComponent } from './panel-board/panel-board.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:PanelBoardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
